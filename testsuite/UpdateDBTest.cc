3258994484e6cd4635ce0e564d269dd368404973
Michael Calmer <mc@suse.de>
2005-08-16 16:13:11 +0000

    add test for updateDB()

---
+++ testsuite/UpdateDBTest.cc
@@ -0,0 +1,86 @@
+#include <blocxx/Logger.hpp>
+#include <blocxx/AppenderLogger.hpp>
+#include <blocxx/CerrLogger.hpp>
+#include <blocxx/CerrAppender.hpp>
+#include <blocxx/String.hpp>
+#include <blocxx/PerlRegEx.hpp>
+#include <limal/Logger.hpp>
+#include <limal/PathInfo.hpp>
+#include <limal/ca-mgm/CA.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <unistd.h>
+
+extern "C" {
+#include <EXTERN.h>
+#include <perl.h>
+}
+
+EXTERN_C void xs_init (pTHX);
+PerlInterpreter *my_perl;
+
+using namespace blocxx;
+using namespace limal;
+using namespace limal::ca_mgm;
+
+limal::Logger logger("UpdateDBTest");
+
+int main()
+{
+    char *embedding[] = { "", "-I../src/", "-MDynaLoader", "-MOPENSSL", "-MOPENSSL::CATools", "-e", 
+                          "0" };
+    
+    PERL_SYS_INIT3(&argc,&argv,&env);
+    my_perl = perl_alloc();
+    perl_construct( my_perl );
+    
+    perl_parse(my_perl, xs_init, 7, embedding, NULL);
+    PL_exit_flags |= PERL_EXIT_DESTRUCT_END;
+    perl_run(my_perl);
+
+
+    try {
+        std::cout << "START" << std::endl;
+        
+        // Logging
+        blocxx::LogAppenderRef	logAppender(new CerrAppender(
+                                                             LogAppender::ALL_COMPONENTS,
+                                                             LogAppender::ALL_CATEGORIES,
+                                                             // category component - message
+                                                             "%-5p %c - %m"
+                                                             ));
+        blocxx::LoggerRef	appLogger(new AppenderLogger(
+                                                         "UpdateDBTest",
+                                                         E_ALL_LEVEL,
+                                                         logAppender
+                                                         ));
+        limal::Logger::setDefaultLogger(appLogger);
+        
+        std::cout << "=================== start Update DB ======================" << std::endl;
+        {
+            CA ca("Test_CA1", "system", "./TestRepos/");
+
+            ca.updateDB();
+        }
+        std::cout << "=================== test wrong password ==================" << std::endl;
+        {
+            CA ca2("Test_CA1", "tralla", "./TestRepos/");
+            
+            ca2.updateDB();
+        }
+        std::cout << "=================== end Update DB ========================" << std::endl;
+
+        std::cout << "DONE" << std::endl;
+    } catch(blocxx::Exception& e) {
+        std::cerr << e << std::endl;
+    }
+
+    perl_destruct(my_perl);
+    perl_free(my_perl);
+    PERL_SYS_TERM();
+
+    return 0;
+}
+
+/* vim: set ts=8 sts=8 sw=8 ai noet: */
