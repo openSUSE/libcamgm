cfb432a95d60080bd57434c5a9e325ad68edce02
Michael Calmer <mc@suse.de>
2005-09-06 14:53:07 +0000

    add overwrite parameter to writeFile()

--- src/limal/ca-mgm/LocalManagement.hpp
+++ src/limal/ca-mgm/LocalManagement.hpp
@@ -95,6 +95,7 @@ namespace CA_MGM_NAMESPACE {
         static void
         writeFile(const ByteArray& data,
                   const String &file,
+                  bool overwrite = true,
                   mode_t mode = 0644);
 
         static String
