cf6a53d3dc7fb5a317abbb7121dfc4171eeba8f4
Michael Calmer <mc@suse.de>
2005-09-22 15:00:39 +0000

    add documentation

--- src/limal/ca-mgm/AuthorityInfoAccessExtension.hpp
+++ src/limal/ca-mgm/AuthorityInfoAccessExtension.hpp
@@ -37,23 +37,75 @@ namespace CA_MGM_NAMESPACE {
     class AuthorityInformation {
 
     public:
+        /**
+         * Construct an empty AuthorityInformation object
+         */
         AuthorityInformation();
-        AuthorityInformation(const AuthorityInformation& ai);
+
+        /**
+         * Construct an object with access OID and location
+         *
+         * @code
+         *
+         *   LiteralValue lv("URI", "http://www.example.com/ca/");
+         *   AuthorityInformation ai("caIssuers", lv);
+         *
+         * @endcode
+         *
+         * @param accessOID <b>OCSP</b>, <b>caIssuers</b> or any valid OID
+         * @param location location of the information
+         *
+         */
         AuthorityInformation(const String &accessOID, 
                              const LiteralValue& location);
 
+        /**
+         * Copy an AuthorityInformation object
+         */
+        AuthorityInformation(const AuthorityInformation& ai);
+
         AuthorityInformation&   operator=(const AuthorityInformation& ai);
 
+        /**
+         * Set new Authority Informations
+         *
+         * @param accessOID <b>OCSP</b>, <b>caIssuers</b> or any valid OID
+         * @param location location of the information
+         *
+         */
         void                    setAuthorityInformation(const String &accessOID, 
                                                         const LiteralValue& location);
 
+        /**
+         * Return the access OID
+         */
         String                  getAccessOID() const;
+
+        /**
+         * Return the location object
+         */
         LiteralValue            getLocation() const;
 
+        /**
+         * Check if this object is valid
+         *
+         * @return true if this object is valid, otherwise false
+         */
         bool                    valid() const;
-        blocxx::StringArray     verify() const;
 
-        blocxx::StringArray     dump() const;
+        /**
+         * Verify this object and return an Array with all
+         * error messages.
+         *
+         * @return Array with error messages. If this Array is empty this
+         * object is valid
+         */
+        blocxx::Array<String>   verify() const;
+
+        /**
+         * Return the content of this object for debugging
+         */
+        blocxx::Array<String>   dump() const;
 
     private:
         String                  accessOID;
@@ -61,27 +113,85 @@ namespace CA_MGM_NAMESPACE {
 
     };
 
+    /**
+     * The authority information access extension gives details
+     * about how to access certain information relating to the CA.
+     *
+     * @code
+     *
+     *   LiteralValue lv("URI", "http://www.example.com/ca/");
+     *   AuthorityInformation ai("caIssuers", lv);
+     *
+     *   List<AuthorityInformation> list;
+     *   list.push_back(ai);
+     *
+     *   AuthorityInfoAccessExtension aie;
+     *   aie.setAuthorityInformation(list);
+     *
+     * @endcode
+     */
     class AuthorityInfoAccessExtension : public ExtensionBase {
     public:
 
+        /**
+         * Construct an empty object
+         */
         AuthorityInfoAccessExtension();
+
         AuthorityInfoAccessExtension(const AuthorityInfoAccessExtension& extension);
+
+        /**
+         * Construct an AuthorityInfoAccessExtension object from a config object
+         *
+         * @param caConfig object of the configuration file
+         * @param type the type describes the section of the config file
+         *
+         */
         AuthorityInfoAccessExtension(CAConfig* caConfig, Type type);
+
         virtual ~AuthorityInfoAccessExtension();
 
         AuthorityInfoAccessExtension& operator=(const AuthorityInfoAccessExtension& extension);
-        
+
+        /**
+         * Set a new list of Authority Informations
+         */
         void
         setAuthorityInformation(const blocxx::List<AuthorityInformation>& infolist);
 
+        /**
+         * Return the list with Authority Informations
+         */
         blocxx::List<AuthorityInformation>
         getAuthorityInformation() const;
 
+        /**
+         * Write the informations of this object back to the configuration file
+         *
+         * @param ca the CA object which holds the config object
+         * @param type the type describes the section of the config file
+         */
         virtual void commit2Config(CA& ca, Type type) const;
 
+        /**
+         * Check if this object is valid
+         *
+         * @return true if this object is valid, otherwise false
+         */
         virtual bool                 valid() const; 
+
+        /**
+         * Verify this object and return an Array with all
+         * error messages.
+         *
+         * @return Array with error messages. If this Array is empty this
+         * object is valid
+         */
         virtual blocxx::StringArray  verify() const;
 
+        /**
+         * Return the content of this object for debugging
+         */
         virtual blocxx::StringArray  dump() const;
 
     private:
--- src/limal/ca-mgm/LocalManagement.hpp
+++ src/limal/ca-mgm/LocalManagement.hpp
@@ -43,6 +43,16 @@ namespace CA_MGM_NAMESPACE {
 
     public:
 
+        /**
+         * Import a certificate to a specific destination
+         *
+         * @param pkcs12File full path to a PKCS12 file with the certificates
+         * @param password the password for the PKCS12 file
+         * @param destinationCAsDir path to the directory where the CAs are stored
+         * @param destinationCertFile the path where the certificate should be stored
+         * @param destinationKeyFile the path where the private key should be stored
+         *
+         */
         static void 
         importAsLocalCertificate(const String &pkcs12File,
                                  const String &password,
@@ -50,6 +60,16 @@ namespace CA_MGM_NAMESPACE {
                                  const String &destinationCertFile,
                                  const String &destinationKeyFile);
         
+        /**
+         * Import a certificate to a specific destination
+         *
+         * @param pkcs12Data PKCS12 certificate data
+         * @param password the password for the PKCS12 file
+         * @param destinationCAsDir path to the directory where the CAs are stored
+         * @param destinationCertFile the path where the certificate should be stored
+         * @param destinationKeyFile the path where the private key should be stored
+         *
+         */
         static void
         importAsLocalCertificate(const limal::ByteBuffer &pkcs12Data,
                                  const String            &password,
@@ -57,50 +77,135 @@ namespace CA_MGM_NAMESPACE {
                                  const String            &destinationCertFile,
                                  const String            &destinationKeyFile);
 
+        /**
+         * Import a certificate as common server certificate.
+         * This function store the CAs to '/etc/ssl/certs', the
+         * certificate to '/etc/ssl/servercerts/servercert.pem' and
+         * the private key to '/etc/ssl/servercerts/serverkey.pem'.
+         *
+         * @param pkcs12File full path to a PKCS12 file with the certificates
+         * @param password the password for the PKCS12 file
+         *
+         */
         static void 
         importCommonServerCertificate(const String &pkcs12File,
                                       const String &password);
         
+        /**
+         * Import a certificate as common server certificate.
+         * This function store the CAs to '/etc/ssl/certs', the
+         * certificate to '/etc/ssl/servercerts/servercert.pem' and
+         * the private key to '/etc/ssl/servercerts/serverkey.pem'.
+         *
+         * @param pkcs12Data PKCS12 certificate data
+         * @param password the password for the PKCS12 file
+         *
+         */
         static void
         importCommonServerCertificate(const limal::ByteBuffer &pkcs12File,
                                       const String            &password);
         
+        /**
+         * Parse a Certificate and return the data
+         *
+         * @param file path to the certificate file in PEM or DER format
+         * @param type the format of the certificate
+         *
+         * @return the parsed certificate data
+         */
         static CertificateData
         getCertificate(const String &file,
                        FormatType    type);
         
+        /**
+         * Parse a Certificate and return the data
+         *
+         * @param data the certificate data in PEM or DER format
+         * @param type the format of the certificate
+         *
+         * @return the parsed certificate data
+         */
+        static CertificateData
+        getCertificate(const limal::ByteBuffer &data,
+                       FormatType               type);
+
+        /**
+         * Parse a Request and return the data
+         *
+         * @param file path to the request file in PEM or DER format
+         * @param type the format of the request
+         *
+         * @return the parsed request data
+         */
         static RequestData
         getRequest(const String &file,
                    FormatType    type);
         
-        static CRLData
-        getCRL(const String &file,
-               FormatType    type);
-
-        static CertificateData
-        getCertificate(const limal::ByteBuffer &data,
-                       FormatType               type);
-        
+        /**
+         * Parse a Request and return the data
+         *
+         * @param data the request data in PEM or DER format
+         * @param type the format of the request
+         *
+         * @return the parsed request data
+         */
         static RequestData
         getRequest(const limal::ByteBuffer &data,
                    FormatType               type);
+
         
+        /**
+         * Parse a CRL and return the data
+         *
+         * @param file path to the CRL file in PEM or DER format
+         * @param type the format of the CRL
+         *
+         * @return the parsed CRL data
+         */
+        static CRLData
+        getCRL(const String &file,
+               FormatType    type);
+        
+        
+        /**
+         * Parse a CRL and return the data
+         *
+         * @param data the CRL data in PEM or DER format
+         * @param type the format of the CRL
+         *
+         * @return the parsed CRL data
+         */
         static CRLData
         getCRL(const limal::ByteBuffer &data,
                FormatType               type);
         
-
+        /**
+         * Read a file from the harddisk and return
+         * the content as ByteBuffer Object
+         *
+         * @param file the path to the file to read
+         *
+         * @return the file content
+         */
         static limal::ByteBuffer
         readFile(const String& file);
 
+        /**
+         * Write data into a file
+         *
+         * @param data the data to write
+         * @param file the path to the file
+         * @param overwrite if this is true and the file exists it will
+         * be overwritten with the new data, if false it throws an exception
+         * @param mode the file permissions for the file (only if it is new created)
+         *
+         */
         static void
         writeFile(const limal::ByteBuffer& data,
                   const String &file,
                   bool overwrite = true,
                   mode_t mode = 0644);
-        
     };
-
 }
 }
 #endif //LIMAL_CA_MGM_LOCAL_MANAGEMENT_HPP
