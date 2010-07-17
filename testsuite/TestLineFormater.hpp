5035d00c4a7b64a459d564777e8278f916011605
Michael Calmer <mc@suse.de>
2010-07-17 16:40:31 +0000

    Special Formater for the testcases

---
+++ testsuite/TestLineFormater.hpp
@@ -0,0 +1,32 @@
+#ifndef LIMAL_TEST_LINE_FORMATER_HPP
+#define LIMAL_TEST_LINE_FORMATER_HPP
+
+#include <iosfwd>
+#include <string>
+#include <limal/LogControl.hpp>
+
+///////////////////////////////////////////////////////////////////
+//namespace ca_mgm
+//{ /////////////////////////////////////////////////////////////////
+
+    struct TestLineFormater : public ca_mgm::LogControl::LineFormater
+    {
+      virtual std::string format( const std::string &      group_r,
+                                  ca_mgm::logger::LogLevel level_r,
+                                  const char *             ,
+                                  const char *             ,
+                                  int                      ,
+                                  const std::string &      message_r )
+      {
+         return ca_mgm::str::form( "%-5s %s - %s",
+                                   ca_mgm::logger::logLevelToString( level_r).c_str(),
+                                   group_r.c_str(), message_r.c_str());
+      }
+      virtual ~TestLineFormater() {}
+    };
+
+
+  /////////////////////////////////////////////////////////////////
+//} // namespace ca_mgm
+///////////////////////////////////////////////////////////////////
+#endif // LIMAL_TEST_LINE_FORMATER_HPP
---
+++ testsuite/TestLineFormater2.hpp
@@ -0,0 +1,41 @@
+#ifndef LIMAL_TEST_LINE_FORMATER2_HPP
+#define LIMAL_TEST_LINE_FORMATER2_HPP
+
+#include <iosfwd>
+#include <string>
+#include <limal/LogControl.hpp>
+
+///////////////////////////////////////////////////////////////////
+//namespace ca_mgm
+//{ /////////////////////////////////////////////////////////////////
+
+    struct TestLineFormater2 : public ca_mgm::LogControl::LineFormater
+    {
+      virtual std::string format( const std::string &      group_r,
+                                  ca_mgm::logger::LogLevel level_r,
+                                  const char *             ,
+                                  const char *             ,
+                                  int                      ,
+                                  const std::string &      message_r )
+      {
+        if(level_r == ca_mgm::logger::E_ERROR)
+        {
+          return ca_mgm::str::form( "%-5s %s - %s",
+                                    ca_mgm::logger::logLevelToString( level_r).c_str(),
+                                    group_r.c_str(), "not log openssl errors for testing");
+        }
+        else
+        {
+          return ca_mgm::str::form( "%-5s %s - %s",
+                                    ca_mgm::logger::logLevelToString( level_r).c_str(),
+                                    group_r.c_str(), message_r.c_str());
+        }
+      }
+      virtual ~TestLineFormater2() {}
+    };
+
+
+  /////////////////////////////////////////////////////////////////
+//} // namespace ca_mgm
+///////////////////////////////////////////////////////////////////
+#endif // LIMAL_TEST_LINE_FORMATER_HPP
